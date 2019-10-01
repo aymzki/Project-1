e335c7cc3f88d803dbe08eec437d886e10feaf7e
maurafortino <maurafortino@gmail.com>
2019-09-30 20:24:56 -0400

    added code

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,162 +1,5 @@
 $(document).ready(function () {
 
-  var mainIngredient;
-  var drinkIngredient;
-  var drinksArray = [];
-  var recipeIngredient;
-  //variables for the chosen food recipe
-  var chosenFoodName;
-  var chosenFoodInstructions;
-  var chosenFoodRecipe;
-  var chosenFoodImage;
-
-  //variables for the chosen drink recipe
-  var chosenDrinkName;
-  var chosenDrinkInstructions;
-  var chosenDrinkRecipe;
-  var chosenDrinkImage;
-
-  function generateRecipes() {
-
-    //on click of button will automatically search for meals with chicken breast as the main ingredient
-    var search = mainIngredient;
-    var queryURL = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" + search;
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-
-      //stores the chicken breast meals response in a variable called results
-      var results = response.meals
-      //for each meal go in and find the mealID
-      for (var i = 0; i < results.length; i++) {
-        var mealID = results[i].idMeal;
-        //search the mealID of each chicken breast meal response
-        var mealURL = "https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=" + mealID;
-        $.ajax({
-          url: mealURL,
-          method: "GET"
-        }).then(function (data) {
-          //for each meal ID get the necessary information
-          var mealResults = data.meals;
-          for (var j = 0; j < mealResults.length; j++) {
-            //store ingredients into an array - not all ingredients will have something listed
-            var ingredients = [mealResults[j].strIngredient1, mealResults[j].strIngredient2, mealResults[j].strIngredient3, mealResults[j].strIngredient4, mealResults[j].strIngredient5, mealResults[j].strIngredient6, mealResults[j].strIngredient7, mealResults[j].strIngredient8, mealResults[j].strIngredient9, mealResults[j].strIngredient10, mealResults[j].strIngredient11, mealResults[j].strIngredient12, mealResults[j].strIngredient13, mealResults[j].strIngredient14, mealResults[j].strIngredient15, mealResults[j].strIngredient16, mealResults[j].strIngredient17, mealResults[j].strIngredient18, mealResults[j].strIngredient19, mealResults[j].strIngredient20,]
-            //store measurements into an array - not all measurements will have something listed
-            var measurements = [mealResults[j].strMeasure1, mealResults[j].strMeasure2, mealResults[j].strMeasure3, mealResults[j].strMeasure4, mealResults[j].strMeasure5, mealResults[j].strMeasure6, mealResults[j].strMeasure7, mealResults[j].strMeasure8, mealResults[j].strMeasure9, mealResults[j].strMeasure10, mealResults[j].strMeasure11, mealResults[j].strMeasure12, mealResults[j].strMeasure13, mealResults[j].strMeasure14, mealResults[j].strMeasure15, mealResults[j].strMeasure16, mealResults[j].strMeasure17, mealResults[j].strMeasure18, mealResults[j].strMeasure19, mealResults[j].strMeasure20,]
-            //store instructions into an array
-            var instructions = mealResults[j].strInstructions;
-            //store images into an array
-            var image = mealResults[j].strMealThumb;
-            //creates a new paragraph to store everything into at the end
-            var parentRow = $("<div>");
-            var leftColDiv = $("<div>");
-            var rightColDiv = $("<div>");
-            var ingredientsHeader = $("<h5>");
-            var instructionsHeader = $("<h5>");
-            var childRow = $("<div>");
-            //creates a new image tag to display each image
-            var displayImage = $("<img>");
-            //gets the index of the ingredient index of the first ingredient with just a space as it's content
-            var newIngredientsArray = [];
-            var newMeasurementsArray = [];
-            var firstSpaceIng = ingredients.indexOf('');
-            var firstEmpty = measurements.indexOf("");
-            var firstSpaceMeas = measurements.indexOf(" ");
-            newIngredientsArray = ingredients.slice(0, firstSpaceIng);
-            if (firstSpaceMeas === -1) {
-              newMeasurementsArray = measurements.slice(0, firstEmpty);
-            } else {
-              newMeasurementsArray = measurements.slice(0, firstSpaceMeas);
-            }
-            //this array combines both the measurements array and the ingredients array based on their indexes
-            var newArray = newMeasurementsArray.map((e, i) => e + " " + newIngredientsArray[i]);
-            //this loops through the new array and stores each combintation in their own div which is appended to the paragraph
-            for (var x = 0; x < newArray.length; x++) {
-              var newDiv = $("<div>");
-              newDiv.text(newArray[x]);
-              rightColDiv.append(newDiv);
-            };
-
-            var mealName = mealResults[j].strMeal;
-            var nameDisplay = $("<h3>").text(mealName);
-            displayImage.attr("src", image);
-            displayImage.attr("data-category", mealResults[0].strCategory);
-            displayImage.addClass("image");
-            displayImage.addClass("z-depth-5");
-
-            //RECIPE BLOCK
-            parentRow.addClass("row px-0");
-
-            //IMAGE DIV CONTAINER
-            leftColDiv.prepend(nameDisplay);
-            leftColDiv.append(displayImage);
-            leftColDiv.addClass("col-6 mb-3 pl-0");
-
-
-            //INGREDIENTS
-            ingredientsHeader.text("Ingredients");
-            instructionsHeader.text("Instructions");
-            rightColDiv.addClass("col-6 mb-3");
-            rightColDiv.prepend(ingredientsHeader);
-            childRow.append(instructions);
-            childRow.prepend(instructionsHeader)
-            childRow.addClass("row mx-0");
-
-            //INSTRUCTIONS
-            $(parentRow).append(leftColDiv, rightColDiv, childRow);
-            $("#recipe-view").append(parentRow);
-          };
-        });
-      };
-    });
-  };
-
-  $("#search-city").on("click", function (event) {
-    event.preventDefault();
-    $("#recipe-view").empty();
-    $("#drinks-view").empty();
-    mainIngredient;
-    var city = $("#city-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-      var maxWeatherTemp = response.main.temp_max;
-      var minWeatherTemp = response.main.temp_min;
-      var avgWeatherTemp = Math.floor((maxWeatherTemp + minWeatherTemp) / 2);
-      console.log(avgWeatherTemp);
-      if (avgWeatherTemp <= 32) {
-        var freezingTempFood = ["potatoes", "beef", "spinach"];
-        var freezingIndex = [Math.floor(Math.random() * freezingTempFood.length)];
-        mainIngredient = freezingTempFood[freezingIndex];
-        console.log(mainIngredient);
-      } else if (avgWeatherTemp > 32 && avgWeatherTemp < 60) {
-        var coldTempFood = ["ginger", "nutmeg", "mushrooms"];
-        var coldIndex = [Math.floor(Math.random() * coldTempFood.length)];
-        mainIngredient = coldTempFood[coldIndex];
-        console.log(mainIngredient);
-      } else if (60 <= avgWeatherTemp && avgWeatherTemp < 80) {
-        var warmTempFood = ["garlic", "carrots", "white wine"];
-        var warmIndex = [Math.floor(Math.random() * warmTempFood.length)];
-        mainIngredient = warmTempFood[warmIndex];
-        console.log(mainIngredient);
-      } else {
-        var hotTempFood = ["tomatoes", "parlsey", "leek"];
-        var hotIndex = [Math.floor(Math.random() * hotTempFood.length)];
-        mainIngredient = hotTempFood[hotIndex];
-        console.log(mainIngredient);
-      };
-      generateRecipes();
-      $("#city-input").val("");
-    });
-  });
-
-  $(document).on("click", ".image", function () {
-
-    $(document).ready(function () {
       var mainIngredient;
       var drinkIngredient;
       var drinksArray = [];
@@ -217,10 +60,8 @@ $(document).ready(function () {
                 var newMeasurementsArray = [];
                 var newArray = [];
                 var firstSpaceIng = ingredients.indexOf('');
-                var firstEmpty = ingredients.indexOf("");
+                var firstEmpty = measurements.indexOf("");
                 var firstSpaceMeas = measurements.indexOf(" ");
-                console.log("space index " + firstSpaceMeas);
-                console.log("empty index" + firstEmpty);
                 newIngredientsArray = ingredients.slice(0, firstSpaceIng);
                 if (firstSpaceMeas === -1) {
                   newMeasurementsArray = measurements.slice(0, firstEmpty);
@@ -480,5 +321,3 @@ $(document).ready(function () {
         console.log(chosenFoodRecipe);
       });
     });
-  });
-});
